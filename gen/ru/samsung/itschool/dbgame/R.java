c2a2f48ab9b98fc6013569e6a2a90ba12e16694b
nikivak <nikivak@mail.ru>
2015-02-18 19:38:31 +0300

    18febsamsung

--- gen/ru/samsung/itschool/dbgame/R.java
+++ gen/ru/samsung/itschool/dbgame/R.java
@@ -30,10 +30,13 @@ public final class R {
     public static final class layout {
         public static final int activity_ho_f=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_stat=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050003;
         public static final int title_activity_ho_f=0x7f050001;
+        public static final int title_activity_stat=0x7f050002;
     }
     public static final class style {
         /** 
